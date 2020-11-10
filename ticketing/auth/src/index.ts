a066dce0d3b2f5dc4ac19bc61432b40f699931f9
Jonathan De Groot <jdegroot2797@gmail.com>
2020-11-09 20:36:18 -0500

    bug in skaffold.yaml
    dockerfile field mispelled, failed to rebuild docker images correctly

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -4,9 +4,9 @@ import { json } from 'body-parser';
 const app = express();
 app.use(json());
 
-app.get('api/users/currentuser', (req, res) =>{
-    res.send("testing");
-})
+app.get('/api/users/currentuser', (req, res) => {
+    res.send('testing!!');
+  });
 
 app.listen(3000, () => {
     console.log('Listening on port 3000!');

a52444fcb0d174db6b9d47b00819523d354bac30
Jonathan De Groot <jdegroot2797@gmail.com>
2020-11-16 23:31:43 -0500

    created validation result error handling middleware to improve code reusability for each route

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
-import { RequestValidationError } from '../errors/request-validation-error'
+
+import { validateRequest } from '../middleware/validate-request';
 import { BadRequestError } from '../errors/bad-request-error';
 import { User } from '../models/user';
 
@@ -18,13 +19,10 @@ router.post('/api/users/signup',
             .isLength({min: 8, max: 80})
             .withMessage('Password must be between 8 to 80 characters')
     ],
+    // validation error handling middleware for code reusability
+    // for all our routes
+    validateRequest,
     async (req: Request, res: Response) => {
-        // check for errors
-        const errors = validationResult(req);
-
-        if(!errors.isEmpty()){
-            throw new RequestValidationError(errors.array());
-        }
         // retrieve email and password from body
         const { email, password } = req.body;
         

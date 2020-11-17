f95f7d2cebb9ce5fe48b2cd0af885258e0338055
Jonathan De Groot <jdegroot2797@gmail.com>
2020-11-17 00:28:44 -0500

    requireAuth middleware implemented for any time unauthorized user requests a route

--- ticketing/auth/src/middleware/require-auth.ts
+++ ticketing/auth/src/middleware/require-auth.ts
@@ -1,9 +1,11 @@
 import {Request, Response, NextFunction} from 'express';
+import { NotAuthError } from '../errors/not-auth-error';
 
 
 export const requireAuth = (req: Request, res: Response, next: NextFunction) => {
     if(!req.currentUser){
-        //TODO: create a not authed error
-        return res.status(401).send({});
+       throw new NotAuthError();
     }
+
+    next();
 }

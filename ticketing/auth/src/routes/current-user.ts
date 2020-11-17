0e9058de75e323233aa12127c998bc28a426c1c4
Jonathan De Groot <jdegroot2797@gmail.com>
2020-11-17 00:01:24 -0500

    tested and working implementation of currentuser route

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,9 +1,29 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-    res.send('Hi there!!!');
+    // is jwt set?
+    if(!req.session?.jwt){
+        return res.send({currentUser: null});
+    }
+    //if so respond that user does not have valid session token
+    try{
+        const payload = jwt.verify(
+            req.session.jwt, 
+            process.env.JWT_KEY!
+        );
+        //otherwise user has valida session token
+        // send back the current user
+        res.send({currentUser: payload});
+    }catch (err){
+        res.send({currentUser: null});
+    }
+    
+    // TODO: create current user middlerware
+    
 });
 
 export { router as currentUserRouter };

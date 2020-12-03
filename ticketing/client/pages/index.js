f6fa4a8f50f34e46c2fb85dc035ae280c507735e
Jonathan De Groot <jdegroot2797@gmail.com>
2020-12-03 17:47:40 -0500

    server side authentication call issue

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,15 +1,26 @@
-const landingPage = ({ test }) => {
-  console.log('i am in the component', test);
+import axios from 'axios';
+
+// this is a component (browser)
+const landingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Hello</h1>;
 };
+
+//this is not a components (server)
 
 // next js will call this function while attempting to render
 // from the server and will be provided to the component as a prop.
 // function will be exected on the server side.
-landingPage.getInitialProps = () => {
-  console.log('server side!');
+landingPage.getInitialProps = async () => {
+  //cannot use hooks outside of components
+  //must use axios within getInitialProps to see whether user is authed or not
 
-  return { test: 'hello world' };
+  //since this is being called from server therefore our client pod in k8s
+  //it never reaches the ingress nginx router, node slaps on a localhost domain,
+  //since this localhost is the pod it results in a connection error.
+  const response = await axios.get('/api/users/currentuser');
+
+  return response.data;
 };
 
 export default landingPage;

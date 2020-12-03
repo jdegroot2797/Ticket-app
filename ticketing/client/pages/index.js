88057a46b62720439c8d974881dabba17c65a7ea
Jonathan De Groot <jdegroot2797@gmail.com>
2020-12-03 17:22:18 -0500

    testing next feature of server side rendering with getInitialProps

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,3 +1,15 @@
-export default () => {
+const landingPage = ({ test }) => {
+  console.log('i am in the component', test);
   return <h1>Hello</h1>;
 };
+
+// next js will call this function while attempting to render
+// from the server and will be provided to the component as a prop.
+// function will be exected on the server side.
+landingPage.getInitialProps = () => {
+  console.log('server side!');
+
+  return { test: 'hello world' };
+};
+
+export default landingPage;

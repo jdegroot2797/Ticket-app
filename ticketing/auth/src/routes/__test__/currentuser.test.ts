04472c42eac55f56f299548f503c4691ea7ecae6
Jonathan De Groot <jdegroot2797@gmail.com>
2020-12-02 19:33:47 -0500

    add unauthenticated user test

--- ticketing/auth/src/routes/__test__/currentuser.test.ts
+++ ticketing/auth/src/routes/__test__/currentuser.test.ts
@@ -12,5 +12,13 @@ it('responds with details of the signed in user', async () => {
 
     //console.log(res.body);
     expect(res.body.currentUser.email).toEqual('test@test.com');
+});
 
+it('responds with 401 status code and response of user being undefined if user not authenticated', async () => {
+    const response = await request(app)
+        .get('/api/users/currentuser')
+        .send()
+        .expect(401);
+
+  expect(response.body.currentUser).toEqual(undefined);
 });

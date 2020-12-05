cecc2e8df1caa53ff40d1496b0ca639e065a981e
Jonathan De Groot <jdegroot2797@gmail.com>
2020-12-05 18:05:39 -0500

    implement simple sign in page, add getInitialProps to _app so we can access all data in the wrapper for pages. This allows something common like a header to be used on each page with all the required information it needs

---
+++ ticketing/client/components/header.js
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+
+export default ({ currentUser }) => {
+  const links = [
+    !currentUser && { label: 'Sign Up', href: '/auth/signup' },
+    !currentUser && { label: 'Sign In', href: '/auth/signin' },
+    currentUser && { label: 'Sign Out', href: '/auth/signout' },
+  ]
+    // filters out false entries from links array
+    .filter((linkConfig) => linkConfig)
+    // map over remaining array elements that are true
+    .map(({ label, href }) => {
+      return (
+        <li key={href} className="navItem">
+          <Link href={href}>
+            <a className="nav-link">{label}</a>
+          </Link>
+        </li>
+      );
+    });
+
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <Link href="/">
+        <a className="navbar-brand">Tix</a>
+      </Link>
+
+      <div className="d-flex justify-content-end">
+        <ul className="nav d-flex align-items-center">{links}</ul>
+      </div>
+    </nav>
+  );
+};
--- ticketing/client/pages/_app.js
+++ ticketing/client/pages/_app.js
@@ -1,10 +1,44 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
+import Header from '../components/header';
 
 // Wrapper around component (our pages) which are displayed on the screen
 // Eg: index.js
 // this app file is guaranteed to include any global css
 // or file when rendered onto the screen
 // src: https://nextjs.org/docs/basic-features/built-in-css-support
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />;
+    </div>
+  );
 };
+
+// AppContext is different from page context in nextjs since
+// _app.js wraps each of our pages. Likewise getInitialProps
+// will act differently between these two scenarios.
+AppComponent.getInitialProps = async (appContext) => {
+  // get information for appComponent
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  let pageProps = {};
+
+  // get information for specific page
+  // can pass this information to header
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  console.log(pageProps);
+
+  // returns and passes data to pageProps in AppComponent
+  return {
+    pageProps,
+    currentUser: data.currentUser,
+  };
+};
+
+export default AppComponent;

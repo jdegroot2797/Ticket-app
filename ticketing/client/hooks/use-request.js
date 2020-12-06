db550012cda198b78e55592f37026af59c0f3ad6
Jonathan De Groot <jdegroot2797@gmail.com>
2020-12-06 14:37:32 -0500

    name use-request hook for next js

--- ticketing/client/hooks/use-request.js
+++ ticketing/client/hooks/use-request.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useState } from 'react';
 
-export default ({ url, method, body, onSuccess }) => {
+const UseRequest = ({ url, method, body, onSuccess }) => {
   // errors state is null until we receive an errors array
   const [errors, setErrors] = useState(null);
 
@@ -33,3 +33,5 @@ export default ({ url, method, body, onSuccess }) => {
 
   return { doRequest, errors };
 };
+
+export default UseRequest;

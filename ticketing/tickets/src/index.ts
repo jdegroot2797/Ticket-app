43a718bb51fad60ca0a1324604f5e1f8774d2fbd
Jonathan De Groot <jdegroot2797@gmail.com>
2020-12-14 19:59:12 -0500

    testing graceful shutdown of NATS client

--- ticketing/tickets/src/index.ts
+++ ticketing/tickets/src/index.ts
@@ -16,6 +16,18 @@ const startUp = async () => {
 
   try {
     await natsWrapper.connect('tix', 'asdsadas', 'http://nats-srv:4222');
+
+    // event handler if client is closed or disconnected from NATS
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed successfully');
+      process.exit();
+    });
+
+    // sig handler to check if listener is interupted or killed
+    // this allows for graceful stan client shutdown
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,

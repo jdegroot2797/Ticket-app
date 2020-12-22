c96f4f1e52606874b22dde163a895641effbc96f
Jonathan De Groot <jdegroot2797@gmail.com>
2020-12-21 19:14:38 -0500

    need to create a function which checks if a ticket is reserved outside the route handler

--- ticketing/orders/src/routes/new.ts
+++ ticketing/orders/src/routes/new.ts
@@ -1,7 +1,15 @@
 import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
-import { requireAuth, validateRequest } from '@jdtix/common';
+import {
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+  OrderStatus,
+  BadRequestError,
+} from '@jdtix/common';
 import { body } from 'express-validator';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
@@ -20,6 +28,45 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    // Find the desired ticket the user is trying to purchase in MongoDB
+    const ticket = await Ticket.findById(ticketId);
+
+    // if ticket cannot be found send back some type of error message
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    // Ensure the ticket is not already reserved
+    // 1. find order in db where the ticket the user is trying to purchase
+    // already exists in an order.
+    // 2. if the ticket is found AND order status is NOT cancelled.
+    //TODO: extract this check outside the route handler
+    const existingOrder = await Order.findOne({
+      ticket: ticket,
+      status: {
+        $in: [
+          OrderStatus.Created,
+          OrderStatus.AwaitingPayment,
+          OrderStatus.Completed,
+        ],
+      },
+    });
+
+    // 3. if we find an order that means the ticket IS reserved
+    if (existingOrder) {
+      throw new BadRequestError('Ticket is already reserved');
+    }
+
+    //(what if many people are all trying to get a popular ticket?)
+
+    // Check the expiration date for the order
+
+    // if all the checks above are successful
+    // build the order object and save it to the database as a document
+
+    // publish an event telling NATS that the order was successfully created
     res.send({});
   },
 );

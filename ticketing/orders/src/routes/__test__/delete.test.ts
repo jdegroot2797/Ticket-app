40975ddf3b91c5e9186567174fec71b3d7e15949
Jonathan De Groot <jdegroot2797@gmail.com>
2020-12-28 19:21:53 -0500

    Delete route implemented and tested

---
+++ ticketing/orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,36 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+import { Order, OrderStatus } from '../../models/order';
+
+it('Order status changed to cancelled', async () => {
+  // create a ticket
+  const ticket = Ticket.build({
+    title: 'asdsadsa',
+    price: 1000,
+  });
+  await ticket.save();
+
+  // get mock user
+  const user = global.testSignin();
+
+  // make request to create an order
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // make a request to cancel the order
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(204);
+
+  // ensure the order is cancelled
+  const cancelledOrder = await Order.findById(order.id);
+  expect(cancelledOrder!.status).toEqual(OrderStatus.Cancelled);
+});
+
+// TODO: create a test to make sure an event emission is made for "deleting an order"

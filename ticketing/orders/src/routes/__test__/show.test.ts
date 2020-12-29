ee2cc4765078bdda86a6ae9948c5285376c8389a
Jonathan De Groot <jdegroot2797@gmail.com>
2020-12-28 19:05:08 -0500

    ensure other users cannot access each other's orders

--- ticketing/orders/src/routes/__test__/show.test.ts
+++ ticketing/orders/src/routes/__test__/show.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'; //
 import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
 
-it('fetches ther order', async () => {
+it('fetches a users orders', async () => {
   // create a ticket
   const ticket = Ticket.build({
     title: 'asdads',
@@ -27,5 +27,33 @@ it('fetches ther order', async () => {
     .send()
     .expect(200);
 
+  // check to see if the order we fetched was the same one created
   expect(fetchedOrder.id).toEqual(order.id);
+});
+
+it('throws an error if one users tries to get another users orders', async () => {
+  // create a ticket
+  const ticket = Ticket.build({
+    title: 'asdads',
+    price: 25,
+  });
+  await ticket.save();
+
+  // get a mock user
+  const user = global.testSignin();
+  const userTwo = global.testSignin();
+
+  // make request to create an order with the newly created ticket
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // try to access newly created order under users #1 with user #2
+  const { body: fetchedOrder } = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', userTwo)
+    .send()
+    .expect(401);
 });

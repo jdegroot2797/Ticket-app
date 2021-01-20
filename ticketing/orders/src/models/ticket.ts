ef77e63483c6b9a89979852f0b9d338139b8beb6
Jonathan De Groot <jdegroot2797@gmail.com>
2021-01-19 20:47:50 -0500

    Implement concurrency to more routes, create function in ticket model to make simpler query for updating tickets.

--- ticketing/orders/src/models/ticket.ts
+++ ticketing/orders/src/models/ticket.ts
@@ -19,7 +19,14 @@ export interface TicketDocument extends mongoose.Document {
 }
 
 interface TicketModel extends mongoose.Model<TicketDocument> {
+  // create a ticket object
   build(attrs: TicketAttributes): TicketDocument;
+
+  // query a ticket by ID and Version by passing in an event object
+  findByEvent(event: {
+    id: string;
+    version: number;
+  }): Promise<TicketDocument | null>;
 }
 
 const ticketSchema = new mongoose.Schema(
@@ -47,6 +54,13 @@ const ticketSchema = new mongoose.Schema(
 // OCC Implementation
 ticketSchema.set('versionKey', 'version');
 ticketSchema.plugin(updateIfCurrentPlugin);
+
+ticketSchema.statics.findByEvent = (event: { id: string; version: number }) => {
+  return Ticket.findOne({
+    _id: event.id,
+    version: event.version - 1,
+  });
+};
 
 ticketSchema.statics.build = (attrs: TicketAttributes) => {
   return new Ticket({
--- ticketing/orders/src/routes/delete.ts
+++ ticketing/orders/src/routes/delete.ts
@@ -27,6 +27,7 @@ router.delete(
     // publish an event for order cancelled
     new OrderCancelledPublisher(natsWrapper.client).publish({
       id: order.id,
+      version: order.ticket.version,
       ticket: {
         id: order.ticket.id,
       },
